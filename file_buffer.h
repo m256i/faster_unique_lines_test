24b9cefbb4d10216638374f7c9421adfa57894f4
manwiththedrip <lespaul05cpt.ne@gmail.com>
2023-10-25 02:03:40 +0200

    initial commit

---
+++ file_buffer.h
@@ -0,0 +1,121 @@
+#pragma once
+#include <iostream>
+#include <string_view>
+#include <vector>
+#include <windows.h>
+#include <cmath>
+
+#include "common.h"
+
+namespace xenon_file
+{
+static usize
+get_fsize(PCTSTR _path)
+{
+  HANDLE hFile{
+      CreateFile(_path, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0)};
+  if (hFile == INVALID_HANDLE_VALUE)
+  {
+    printf("Error opening file: %s\n", _path);
+    return FALSE;
+  }
+  return (usize)GetFileSize(hFile, 0);
+}
+
+static bool
+read_file(PCTSTR _path, std::string& _buf) noexcept
+{
+  // nice way to avoid a goto and its practically as fast
+  constexpr auto _out = [](bool _res, const HANDLE& _file, const std::vector<OVERLAPPED>& _ovls,
+                           const u32& _div, const std::string& _buf)
+  {
+    for (usize i{0}; i != _div; ++i)
+    {
+      if (_ovls[i].hEvent)
+      {
+        CloseHandle(_ovls[i].hEvent);
+      }
+    }
+    CloseHandle(_file);
+    constexpr static usize cacheline_size{64};
+    for (usize i{0}; i != _buf.size() / cacheline_size; ++i)
+    {
+      _mm_prefetch((b8*)&_buf[i * 64u], _MM_HINT_NTA);
+    }
+    return _res;
+  };
+
+  u32 _num_reads{0};
+  HANDLE _file{
+      CreateFile(_path, GENERIC_READ, FILE_SHARE_READ, 0, OPEN_EXISTING, FILE_FLAG_OVERLAPPED, 0)};
+
+  // check if the handle is valid so if the opening of the
+  // file was succesful
+  if (_file == INVALID_HANDLE_VALUE)
+  {
+    return false;
+  }
+  // obtain file size in bytes
+  u32 _fsize{GetFileSize(_file, 0u)};
+  u32 _div{(u32)std::log2(_fsize) / 2u};
+
+  if (_buf.size() <= _fsize) [[unlikely]]
+  {
+    _buf.resize(_fsize);
+  }
+
+  // get the first divisor that the size is
+  // cleanly divisible through
+  // @todo: prime numbers lol, also we dont
+  //    always want the same amount of "threads"
+  while ((_fsize / _div) * _div != _fsize)
+  {
+    _div++;
+  }
+  // @note:   chunk size for async reading
+  //          pass this to ReadFile for every handle
+  u32 _blk_size{_fsize / _div}; // size of each block, in bytes
+
+  std::vector<OVERLAPPED> _ovls(_div);
+  std::vector<HANDLE> _events(_div);
+
+  for (usize i{0}; i != _div; ++i)
+  {
+    _ovls[i].Offset = (DWORD)(i * _blk_size);
+    if (_ovls[i].hEvent = CreateEvent(0u, true, false, 0u); !_ovls[i].hEvent)
+    {
+      return _out(false, _file, _ovls, _div, _buf);
+    }
+    if (!ReadFile(_file, &_buf[i * _blk_size], _blk_size, 0, &_ovls[i]))
+    {
+      if (GetLastError() != ERROR_IO_PENDING)
+      {
+        CancelIo(_file);
+        return _out(false, _file, _ovls, _div, _buf);
+      }
+    }
+  }
+  // make a buffer of events
+  for (usize i{0}; i != _div; ++i)
+  {
+    _events[i] = _ovls[i].hEvent;
+  }
+  _num_reads = _div;
+  // retrieve the events
+  do
+  {
+    u32 _wait_res{WaitForMultipleObjects(_num_reads, &_events[0], false, INFINITE)};
+    if (_wait_res == WAIT_FAILED)
+    {
+      CancelIo(_file);
+      return _out(false, _file, _ovls, _div, _buf);
+    }
+    if ((_wait_res >= WAIT_OBJECT_0) && (_wait_res < (WAIT_OBJECT_0 + _num_reads)))
+    {
+      --_num_reads;
+      if (_num_reads == 0) break;
+    }
+  } while (true);
+  return _out(false, _file, _ovls, _div, _buf);
+}
+} // namespace xenon_file
---
+++ main.cpp
@@ -0,0 +1,136 @@
+// Type your code here, or load an example.
+#include <array>
+#include <bitset>
+#include <cstdint>
+#include <cstring>
+#include <emmintrin.h>
+#include <immintrin.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#include <chrono>
+
+#include <stdint.h>
+#include <highwayhash/highwayhash.h>
+
+#include "file_buffer.h"
+
+uint64_t
+rdtsc()
+{
+  return __rdtsc();
+}
+
+const std::array<char, sizeof(__m256i)> newlines = {
+    '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n',
+    '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n', '\n'};
+
+std::uint32_t
+fast_getline(const char *_str)
+{
+  unsigned long long index   = 0;
+  const __m256i all_newlines = _mm256_loadu_si256((__m256i *)newlines.data());
+  __m256i hits = _mm256_cmpeq_epi8(_mm256_loadu_si256((__m256i *)(_str + index)), all_newlines);
+  std::uint32_t outmask = _mm256_movemask_epi8(hits);
+  while (outmask == 0)
+  {
+    index += sizeof(__m256i);
+    hits    = _mm256_cmpeq_epi8(_mm256_loadu_si256((__m256i *)(_str + index)), all_newlines);
+    outmask = _mm256_movemask_epi8(hits);
+  }
+  return __builtin_ctz(outmask) + index;
+}
+
+bool
+strcmp_(const char *__restrict__ a, const char *__restrict__ b, unsigned long long len)
+{
+  unsigned long long c = 0;
+  while (a[c] == b[c] && (a[c] != '\n' && b[c] != '\n'))
+  {
+    c++;
+  }
+  return c == len;
+}
+
+int
+main([[maybe_unused]] int argc, [[maybe_unused]] char **argv)
+{
+  using std::chrono::duration;
+  using std::chrono::duration_cast;
+  using std::chrono::high_resolution_clock;
+  using std::chrono::milliseconds;
+
+  usize counter = 0, collision_counter = 0;
+  std::string ipt_buf;
+
+#ifdef _WIN32
+
+  auto fsize = xenon_file::get_fsize(TEXT("D:/git/faster_unique/invalid_tokens.txt"));
+
+  ipt_buf.reserve(fsize);
+
+  xenon_file::read_file(TEXT("D:/git/faster_unique/invalid_tokens.txt"), ipt_buf);
+
+#else
+
+  auto stream = std::ifstream(argv[1], std::ios::binary);
+  auto buf    = std::stringstream();
+
+  stream.seekg(0, std::ios::end);
+  size_t size = stream.tellg();
+  std::string buffer(size, ' ');
+  stream.seekg(0);
+  stream.read(&buffer[0], size);
+  ipt_buf = buffer;
+
+#endif
+
+  highwayhash::HHResult64 result;
+
+  const auto mapsize = (usize)(fsize * 1.5);
+  std::vector<uptr> hashmap(mapsize);
+
+  u64 current_index = 0, next_index = 0;
+
+  auto t1 = high_resolution_clock::now();
+
+  _mm_lfence();
+  _mm_prefetch((char *)hashmap.data(), _MM_HINT_T1);
+
+  HH_ALIGNAS(32) const highwayhash::HHKey key = {1, 2, 3, 4};
+  while (current_index < fsize) [[likely]]
+  {
+    next_index = current_index + fast_getline((const char *)((uptr)ipt_buf.data() + current_index));
+
+    highwayhash::HHStateT<HH_TARGET_AVX2> state(key);
+    highwayhash::HighwayHashT(&state, ipt_buf.data() + current_index, next_index - current_index,
+                              &result);
+
+    auto hashvalue = result % mapsize;
+    while (true) [[unlikely]]
+    {
+      if (hashmap[hashvalue] == 0) [[likely]]
+      {
+        hashmap[hashvalue] = (uptr)(ipt_buf.data() + current_index);
+        ++counter;
+        break;
+      }
+      if (strcmp_(ipt_buf.data() + current_index, (const char *)(void *)hashmap[hashvalue],
+                  next_index - current_index) == true) [[unlikely]]
+      {
+        break;
+      }
+      hashvalue = (hashvalue + 1) % mapsize;
+    }
+    current_index = next_index + 1;
+  }
+
+  // auto after = rdtsc();
+  auto t2 = high_resolution_clock::now();
+
+  /* Getting number of milliseconds as a double. */
+  duration<double, std::milli> ms_double = t2 - t1;
+
+  std::cout << "unique lines: " << counter << "  " << (ms_double.count() / 1000.f) << "s\n";
+  std::cout << "collisions: " << collision_counter << "\n";
+}
